645d7db2e1775af078292118197e476b25d4a2e9
Connor Chang <connor@DESKTOP-0U8J1QM.localdomain>
2021-11-16 02:21:48 +0800

    reorganize and add stuff, segTreeSparse WIP

--- check.sh
+++ check.sh
@@ -1,4 +1,3 @@
-alias cfast='g++ -std=c++17 -Wshadow -Wall -DLOCAL -O3'
 csafe='g++ -std=c++17 -Wshadow -Wall -DLOCAL -fsanitize=address -fsanitize=undefined -D_GLIBCXX_DEBUG -g'
 
 $csafe segTree.cpp
